8c93fc3c6964b127f2d4b8a526a1b3931b3f6133
Vini-Assuncao <vinicius.a.santos24@aluno.senai.br>
2025-04-08 08:57:44 -0300

    Coloquei linhas sobre a compra do mercado

--- Projeto_Calculadora/calculadora.py
+++ Projeto_Calculadora/calculadora.py
@@ -1,3 +1,7 @@
+print("FAZER COMPRAS NO MERCADO")
+print()
+
+def mercado(compra1,compra2,preco):
     if ope == "+" or ope == "-" or ope == "*" or ope == "/":
         if ope == "+":
             result = num1 + num2
@@ -11,3 +15,7 @@
     else:
         return "XX Operação inválida... XX"
     
+compra1 = input("Digite a sua primeira compra: ")
+compra2 = input("Digite a sua primeira compra: ")
+preco = float(input("Qual o preço da sua compra? "))
+print(f"O resultado da sua compra no super-mercado é {mercado(compra1,compra2,preco)}")   
